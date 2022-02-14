ce6ce2d242bd4eee90770359d8ed8340d54deadd
Naya <44099724+nayamairena@users.noreply.github.com>
2022-02-13 21:55:16 -0800

    01-primes done.

--- hw4/01-primes.js
+++ hw4/01-primes.js
@@ -1,5 +1,20 @@
+function primeCheck(num) {
+  if(num === 1 || num === 2)
+    return false;
+  for(let j = 2; j < num; j++){
+    if(num % j == 0)
+      return false;
+  }
+  return true;
+}
+
 const getPrimes = () => {
   // Add your code here
+  for(let i = 1; i <= 100; i++){
+    if(primeCheck(i))
+      console.log(i);
+  }
+
 };
 
 getPrimes();

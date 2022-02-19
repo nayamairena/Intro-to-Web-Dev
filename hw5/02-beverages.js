025a8659df61b6df5b5fe3f145ccde612bc71c4c
Naya <44099724+nayamairena@users.noreply.github.com>
2022-02-19 00:01:08 -0800

    progress with 02

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -3,8 +3,41 @@ const url =
 
 let app = document.querySelector('#results');
 
+const addDrinkToDOM = (drink) => {
+  let element = document.createElement('div');
+  let name = document.createElement('p');
+  let pic = document.createElement('img');
+
+  name.textContent = drink.strDrink;
+  pic.src = drink.strDrinkThumb;
+
+  element.append(name);
+  element.append(pic);
+
+  app.append(element);
+}
+
 const fetchData = (url) => {
   // Add your code here
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+
+      var tmp = Object.entries(data);
+      console.log(typeof(tmp));
+      tmp.forEach((array) => {
+        array[1].forEach((drink) => {
+        console.log(drink.strDrink, drink.strDrinkThumb);
+        addDrinkToDOM(drink);
+        });
+      });
+    })
+
+  .catch((error) => console.log(error));
+
+
 };
 
 fetchData(url);

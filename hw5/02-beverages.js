047a606010f0506471551fbc043cc91064fcca88
Naya <44099724+nayamairena@users.noreply.github.com>
2022-02-19 20:50:51 -0800

    Exercise 02 done

--- hw5/02-beverages.js
+++ hw5/02-beverages.js
@@ -6,14 +6,29 @@ let app = document.querySelector('#results');
 
 const addDrinkToDOM = (drink) => {
   let element = document.createElement('div');
-  let name = document.createElement('p');
+  element.setAttribute('class', 'figure');
   let pic = document.createElement('img');
+  let name = document.createElement('h1');
 
-  name.textContent = drink.strDrink;
   pic.src = drink.strDrinkThumb;
+  name.textContent = drink.strDrink;
 
-  element.append(name);
+  app.style.display = "flex";
+  app.style.flexFlow = "row wrap";
+  app.style.justifyContent = "center";
+
+  element.style.margin = "10px 10px 10px 10px";
+
+  name.style.margin = "20px 10px 20px 23px";
+  name.style.fontSize = "1.5em";
+  name.style.width = "200px";
+ 
+
+  pic.style.width = "250px";
+  pic.style.height = "250px";
+
   element.append(pic);
+  element.append(name);
 
   app.append(element);
 }
@@ -24,6 +39,7 @@ const fetchData = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
     console.log('Fetch all drinks');
+    console.log(data);
 
     data.drinks.forEach((item) => {
       addDrinkToDOM(item);
@@ -31,7 +47,16 @@ const fetchData = async (url) => {
     console.groupEnd();
   }catch (error){
     console.error('Request failed', error);
+
+    let element = document.createElement('div');
+    element.textContent = 'An error occured. Please reload the page.';
+    app.append(element);
+
+  }finally{
+    let loader = document.querySelector('#loading');
+    app.removeChild(loader);
   }
+
 };
 
 fetchData(url);
